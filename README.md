f1f2e881bfcc093d93a497723bfb75c9e3f650e3
gnozebac <magbmail@gmail.com>
2021-11-05 12:59:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# template-2n-poo-tarea-s4" 
---
+++ src/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+    public static void main(String[] args) {
+
+        // Imprimir nombre de instructor o cliente y si esta disponible para competir
+        // ejemplo: El instructor xxx SI esta disponible
+
+        //Crear un cliente. Luego mostrar su mombre y que tamano de casillero tiene
+        // ejemplo: El cliente xxx tiene asignado un casillero GRANDE
+
+        // Crear una lista de competencia, agregar clientes a esta lista. Listar los clientes que forman esta lista.
+        // ejemplo: La lista xxxxxx tiene los siguientes competidores: Cliente1, Cliente2, etc..
+
+
+    }
+}
